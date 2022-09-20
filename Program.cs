e3dbd587a2267fd3cd0105d413aa0beb378b74fe
Richard Kohl <zeisigdm@gmail.com>
2022-09-20 10:53:29 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiederholung_Methoden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Wurzel(2));
+        }
+
+        static double Wurzel(double zahl)
+        {
+            double untereGrenze = 1;
+            double obereGrenze = 2;
+            double result;
+            double mitte = (obereGrenze + untereGrenze) / 2;
+            for(int k = 0; k < 100; k++)
+            {
+                mitte = (obereGrenze + untereGrenze) / 2;
+                if (mitte*mitte == 2)
+                {
+                    result = mitte;
+                    break;
+                }
+
+                else
+                {
+                    if(mitte*mitte < 2)
+                    {
+                        untereGrenze = mitte;
+                    }
+                    else
+                    {
+                        obereGrenze = mitte;
+                    }
+                }
+
+            }
+            Console.WriteLine("oG = " + obereGrenze);
+            Console.WriteLine("uG = " + untereGrenze);
+            Console.WriteLine("mitte = " + mitte);
+            result = mitte;
+            return result;
+
+        }
+
+    }
+}
